ce80c19db4ca785f1cc1322630535167b4e68e1a
Daniel Lundqvist <daniel@malarhojden.nu>
2017-10-19 21:36:11 +0100

    llbcp: Re-arrange CMake files a bit.
    Front the extra stuff with LL.cmake. Add wrapper to add_executable that adds the extra warnings.
    
    [git-p4: depot-paths = "//llbcpl/main/": change = 684]

--- cmake/BuildType.cmake
+++ cmake/BuildType.cmake
@@ -3,10 +3,10 @@
 # license that can be found in the LICENSE file.
 
 # Adapted from various CMake files floating around.
-set(__default_build_type "Debug")
+set(__ll_default_build_type "Debug")
 
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-  set(CMAKE_BUILD_TYPE "${__default_build_type}" CACHE
+  set(CMAKE_BUILD_TYPE "${__ll_default_build_type}" CACHE
       STRING "Chose type of build." FORCE)
   set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
                "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
---
+++ cmake/LL.cmake
@@ -0,0 +1,39 @@
+# Copyright 2017 Daniel Lundqvist. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+include(CheckCXXCompilerFlag)
+
+set(__ll_gnu_like_compilers "AppleClang;Clang;GNU"
+    CACHE INTERNAL
+    "List of compiler IDs that have GCC like command line options")
+
+include(BuildType)
+include(Warnings)
+
+function(ll_add_cxx_compiler_flag target scope flag)
+  set(var ${flag})
+  string(REPLACE "-" "_" var ${var})
+  string(REPLACE "/" "_" var ${var})
+  string(REPLACE "=" "_" var ${var})
+  string(TOUPPER ${var} var)
+  string(TOUPPER ${PROJECT_NAME} pu)
+  set(var "LL_CXX_FLAG${var}")
+  check_cxx_compiler_flag(${flag} ${var})
+  if(${var})
+    if(NOT DEFINED ARGV3)
+      target_compile_options(${target} ${scope} ${flag})
+    else()
+      target_compile_options(${target} ${scope} $<$<CONFIG:${ARGV3}>:${flag}>)
+    endif()
+  endif()
+endfunction()
+
+function(ll_add_executable target)
+  add_executable(${target} ${ARGN})
+  target_compile_features(${target} PRIVATE cxx_std_14)
+  set_target_properties(${target} PROPERTIES
+    CXX_STANDARD_REQUIRED ON
+    CXX_EXTENSIONS OFF)
+  ll_target_extra_warnings(${target} PRIVATE)
+endfunction()
